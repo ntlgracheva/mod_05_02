8f9f4bbba9ffbe04b1d9e5f0797d977d4fba47e8
Natalia Gracheva <ntlgracheva@gmail.com>
2023-06-21 12:44:39 +0200

    script added

--- sript/script.js
+++ sript/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const modalTitle = document.querySelector('.form__title');
+console.log(modalTitle);
+
+const buttonFormClose = document.querySelector('.form__close');
+console.log(buttonFormClose);
+
+const formWindow = document.querySelector('.form');
+console.log(formWindow);
+
+const formCheckbox= document.querySelector('.form__checkbox');
+console.log(formCheckbox);
+
+const formDiscount= document.querySelector('#discount-text');
+console.log(formDiscount);
+
+const formFinalePrice= document.querySelector('.final-price-number');
+console.log(formFinalePrice);
